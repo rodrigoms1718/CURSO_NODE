ec92887f65438cf8e8a633865e4925f03a0aa3b1
rodrigoms1718 <rodrigoms1718@gmail.com>
2019-03-21 20:50:40 -0300

    Criando a conexão com o MySQL e recuperando dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,16 @@
 module.exports = function (app) {
     app.get('/noticias', function (req, res) {
-        res.render('noticias/noticias');
+        let mysql = require('mysql');
+        let connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'portal_noticias'
+        });
+
+        connection.query('select * from noticias', function (error, result) {
+            res.send(result);
+        });
+        //res.render('noticias/noticias');
     });
 }
