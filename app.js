f7e85acc9ba8d0ecad0f2acd404d77c5df383e8f
rodrigoms1718 <rodrigoms1718@gmail.com>
2019-03-21 02:29:26 -0300

    Reestruturando a aplicação e criando um módulo para definição das rotas

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 var app = require('./config/server');
-app.get('/', function (req, res) {
-    res.render('home/index');
-});
-app.get('/formulario_inclusao_noticia', function (req, res) {
-    res.render('admin/form_add_noticia');
-});
 
-app.get('/noticias', function (req, res) {
-    res.render('noticias/noticias');
-});
-
+var rotaHome = require('./app/routes/home')(app);
+var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaFormularioInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function () {
     console.log('servidor ON');
