f19b05f642a9af49d6aa058c6d382c013d67575b
rodrigoms1718 <rodrigoms1718@gmail.com>
2019-03-21 22:28:42 -0300

    Refactoring do projeto prático portal de notícias com Consign (routes)

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var app = require('./config/server');
+let app = require('./config/server');
 
-var rotaHome = require('./app/routes/home')(app);
-var rotaNoticias = require('./app/routes/noticias')(app);
-var rotaFormularioInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
+//var rotaHome = require('./app/routes/home')(app);
+//var rotaNoticias = require('./app/routes/noticias')(app);
+//var rotaFormularioInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function () {
     console.log('servidor ON');
--- config/server.js
+++ config/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
+let consign = require('consign');
+
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views')
+
+//varre todas as rotas do app
+consign().include('app/routes').into(app);
 
 module.exports = app;
