e5c25beb3cc956b810b402cf89f939b13a4b0684
rodrigoms1718 <rodrigoms1718@gmail.com>
2019-03-21 21:02:19 -0300

    EJS - Criando views dinâmicas com JavaScript

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -9,8 +9,8 @@ module.exports = function (app) {
         });
 
         connection.query('select * from noticias', function (error, result) {
-            res.send(result);
+            res.render('noticias/noticias', { noticias: result });
         });
-        //res.render('noticias/noticias');
+
     });
 }
