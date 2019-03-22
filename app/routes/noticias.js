3f8e6c4faddfc22f9ed273382ddea69616fa0294
rodrigoms1718 <rodrigoms1718@gmail.com>
2019-03-21 21:11:38 -0300

    refatoring do metodo de conexao com o banco de dados mySql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,13 +1,8 @@
-module.exports = function (app) {
-    app.get('/noticias', function (req, res) {
-        let mysql = require('mysql');
-        let connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'portal_noticias'
-        });
+let dbConnection = require('../../config/dbConnection');
 
+module.exports = function (app) {
+    let connection = dbConnection();
+    app.get('/noticias', function (req, res) {
         connection.query('select * from noticias', function (error, result) {
             res.render('noticias/noticias', { noticias: result });
         });
---
+++ config/dbConnection.js
@@ -0,0 +1,10 @@
+let mysql = require('mysql');
+
+module.exports = function () {
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'portal_noticias'
+    });
+}
